f0ff627c706e8ef250a5259445a77a45c5b8ddb3
Rigel9802 <luckyrigel9802@gmail.com>
2022-07-16 22:05:00 +0700

    test

--- Android Studio/BookAppLibrary/app/src/main/java/com/example/bookapplibrary/MyDatabaseHelper.java
+++ Android Studio/BookAppLibrary/app/src/main/java/com/example/bookapplibrary/MyDatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.bookapplibrary;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -29,7 +31,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
                         " ("+COLUMN_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         COLUMN_TITLE +" TEXT, "+
                         COLUMN_AUTHOR +" TEXT, "+
-                        COLUMN_AUTHOR +" INTEGER);";
+                        COLUMN_PAGES +" INTEGER);";
         db.execSQL(query);
     }
 
@@ -37,5 +39,20 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    void addBook(String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_AUTHOR, author);
+        cv.put(COLUMN_PAGES, pages);
+        long result = db.insert(TABLE_NAME, null, cv);
+        if (result == -1){
+            Toast.makeText(context, "Failed!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Added Successfully!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
